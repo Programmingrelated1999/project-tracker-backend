648322d11d163e3786230f609427f13e02dcb9dd
Kaung Htet Myat <programmingrelated1999@gmail.com>
2022-11-01 22:34:19 -0400

    heroku push

--- utils/config.js
+++ utils/config.js
@@ -5,7 +5,7 @@ const PORT = process.env.PORT || 3001;
 
 //Mongodb uri
 const MONGODB_URI = process.env.NODE_ENV === 'test' 
-  ? process.env.TEST_MONGODB_URI
+  ? "mongodb+srv://khmyat1999:Pyaepyae710520@project-management.f5ztsex.mongodb.net/ProjectApp?retryWrites=true&w=majority"
   : process.env.MONGODB_URI
 
 //export mongodb uri and port
